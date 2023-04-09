303c1b629d544702d164a7a327927d99188967c9
Danil Yudov <skeptick13@gmail.com>
2023-04-10 04:53:52 +0500

    JS implementation of painterResource

---
+++ libres-compose/src/jsMain/kotlin/io/github/skeptick/libres/compose/JsSvgPainter.kt
@@ -0,0 +1,79 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package io.github.skeptick.libres.compose
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.isSpecified
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
+import org.jetbrains.skia.svg.*
+import kotlin.math.ceil
+
+internal class JsSvgPainter(private val dom: SVGDOM, density: Density) : Painter() {
+
+    private val srcSize: Size = dom.pxSize ?: Size.Unspecified
+
+    private lateinit var cachedImage: ImageBitmap
+    private lateinit var cachedCanvas: Canvas
+    private var cachedSize: Size = Size.Unspecified
+    private val cacheScope: CanvasDrawScope = CanvasDrawScope()
+
+    override val intrinsicSize = if (srcSize.isSpecified) srcSize * density.density else Size.Unspecified
+
+    private var alpha: Float = 1.0f
+
+    private var colorFilter: ColorFilter? = null
+
+    override fun applyAlpha(alpha: Float): Boolean {
+        this.alpha = alpha
+        return true
+    }
+
+    override fun applyColorFilter(colorFilter: ColorFilter?): Boolean {
+        this.colorFilter = colorFilter
+        return true
+    }
+
+    override fun DrawScope.onDraw() {
+        if (cachedSize != size) prepareImage()
+        drawImage(cachedImage, srcSize = cachedSize.toIntSize(), alpha = alpha, colorFilter = colorFilter)
+    }
+
+    private fun DrawScope.prepareImage() {
+        cachedImage = ImageBitmap(size.toIntSize())
+        cachedCanvas = Canvas(cachedImage)
+        cachedSize = size
+        cacheScope.clearAndDrawSvg(density = this, layoutDirection, size)
+        cachedImage.prepareToDraw()
+    }
+
+    private fun CanvasDrawScope.clearAndDrawSvg(density: Density, layoutDirection: LayoutDirection, size: Size) {
+        draw(density, layoutDirection, cachedCanvas, size) {
+            drawRect(color = Color.Black, blendMode = BlendMode.Clear)
+            drawIntoCanvas { canvas ->
+                dom.root?.width = SVGLength(size.width, SVGLengthUnit.PX)
+                dom.root?.height = SVGLength(size.height, SVGLengthUnit.PX)
+                dom.root?.preserveAspectRatio = SVGPreserveAspectRatio(SVGPreserveAspectRatioAlign.NONE)
+                dom.render(canvas.nativeCanvas)
+            }
+        }
+    }
+
+}
+
+private inline fun ImageBitmap(intSize: IntSize) = ImageBitmap(intSize.width, intSize.height)
+
+private inline fun Size.toIntSize() = IntSize(ceil(width).toInt(), ceil(height).toInt())
+
+private inline val SVGDOM.pxSize: Size?
+    get() = root?.run {
+        val width = width.withUnit(SVGLengthUnit.PX).value
+        val height = height.withUnit(SVGLengthUnit.PX).value
+        if (width != 0f && height != 0f) Size(width, height) else Size.Unspecified
+    }
--- libres-compose/src/jsMain/kotlin/io/github/skeptick/libres/compose/actuals.kt
+++ libres-compose/src/jsMain/kotlin/io/github/skeptick/libres/compose/actuals.kt
@@ -1,10 +1,55 @@
 package io.github.skeptick.libres.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.platform.LocalDensity
 import io.github.skeptick.libres.images.Image
+import kotlinx.browser.window
+import kotlinx.coroutines.await
+import org.jetbrains.skia.Data
+import org.jetbrains.skia.svg.SVGDOM
+import org.jetbrains.skia.Image as SkiaImage
+import org.khronos.webgl.Int8Array
+import org.w3c.fetch.Response
 
 @Composable
 actual fun painterResource(image: Image): Painter {
-    throw NotImplementedError()
+    val state by produceState<ByteArray?>(initialValue = null, image) {
+        val response = window.fetch(image).await()
+        if (response.ok) value = response.byteArray()
+    }
+
+    return when (val bytes = state) {
+        null -> ColorPainter(color = Color.Transparent)
+        else -> when (image.substringAfterLast(".")) {
+            "svg" -> rememberSvgResource(image, bytes)
+            else -> rememberBitmapResource(image, bytes)
+        }
+    }
 }
+
+@Composable
+private fun rememberSvgResource(name: String, bytes: ByteArray): Painter {
+    val density = LocalDensity.current
+    return remember(name, density) {
+        val data = Data.makeFromBytes(bytes)
+        JsSvgPainter(SVGDOM(data), density)
+    }
+}
+
+@Composable
+private fun rememberBitmapResource(name: String, bytes: ByteArray): Painter {
+    return remember(name) {
+        val skiaImage = SkiaImage.makeFromEncoded(bytes = bytes)
+        BitmapPainter(image = skiaImage.toComposeImageBitmap())
+    }
+}
+
+private suspend fun Response.byteArray() = arrayBuffer().await().let(::Int8Array).unsafeCast<ByteArray>()
